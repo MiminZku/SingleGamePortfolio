28afb8a9155638bc2edda2cc93ee129448ac316a
MiminZku <rkdalsrl111@gmail.com>
2024-07-25 13:04:13 +0900

    Imported Hercules Character

--- Source/SingleGamePortfolio/SingleGamePortfolioCharacter.cpp
+++ Source/SingleGamePortfolio/SingleGamePortfolioCharacter.cpp
@@ -33,7 +33,7 @@ ASingleGamePortfolioCharacter::ASingleGamePortfolioCharacter()
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
 	// instead of recompiling to adjust them
 	GetCharacterMovement()->JumpZVelocity = 700.f;
-	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->AirControl = 0.f;
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
 	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
